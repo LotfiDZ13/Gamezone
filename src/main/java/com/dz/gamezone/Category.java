b892452343db8791523132ad92b1ea0fc0c6a2c8
LotfiDZ13 <lotfichabounia@gmail.com>
2026-03-12 12:31:20 +0100

    Delete src directory

--- src/main/java/com/dz/gamezone/Category.java
+++
@@ -1,7 +0,0 @@
-package com.dz.gamezone;
-import com.google.gson.annotations.SerializedName;
-
-public class Category {
-    @SerializedName("name") public String name;
-    @SerializedName("logo") public String logo;
-}
